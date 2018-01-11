6ef52faf94fca71579685214e652a55b8566983b
Brian Bruno <brian.bruno@outlook.com>
2018-01-11 18:02:13 -0200

    Dashboard Vue

---
+++ resources/assets/js/components/Dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+         <div class="row">
+        <div class="col s12">
+          <div class="card">
+            <div class="card-image">
+              <span class="card-title">Cash Manager</span>
+            </div>
+            <div class="card-content">
+              <p>
+								Investimentos hoje:
+							</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
